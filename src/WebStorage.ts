2cc4042b90aa8dc14fc8308220ccedf71a45b317
DESKTOP-27L9P47\davis <davis@sylvesterllc.com>
2017-01-28 16:47:16 -0600

    Working on Listing  all keys!

--- src/WebStorage.ts
+++ src/WebStorage.ts
@@ -34,7 +34,7 @@ export class WebStorage {
         this.tempStorage.clear();
     }
 
-    public ShowAll(): Array<
+    public ShowAll(): Array<[]>{}
 
     public GetAllKeys(): Array<string> {
         if (this.tempStorage.length === 0) {
